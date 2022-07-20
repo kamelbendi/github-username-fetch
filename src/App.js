f58264274566a18246dbab03d033207ca9292b7e
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-07-20 19:12:50 +0200

    children classes test within functions

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import "./App.css";
-import { useState } from "react";
-import { useEffect } from "react";
 
+function App({children}) {
 
-function FetchAPI() {
-
-  const [response, setResponse] = useState(null);
-  const [userName, setUserName] = useState(null);
-  console.log("whats username", userName); 
-
-  async function getUser(username) {
-    const resp = await fetch(`https://api.github.com/users/${username}`);
-    return await resp.json();
-  }
+return (
+<div>
+  <h1>Hello</h1>
+  {children}
   
-  useEffect( async () => {
-    const user = await getUser(userName);
-    setResponse(user);
-  }, [userName]);  
-  
-  
-  return (
-    <div>
-
-      <p>hello</p>
-      <input
-         value={userName}
-         
-        onChange={(e) => setUserName(e.target.value)} 
-      ></input>
-
-        <h1>Name : {response.username}</h1> 
-        <img src={response.avatar_url}>this is the image</img> 
-       <p>Location: {response.location}</p>  
-    </div>
-  );
+  </div>
+)
 }
 
-
-export default FetchAPI;
+export default App;
